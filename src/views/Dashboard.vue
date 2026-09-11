07be501243bfeb7aa68eaac512a64eb726f6f738
Alaa Tellat <alaa.tellat@slalom.com>
2026-09-11 14:47:50 -0500

    Scaffold Vue project

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="dashboard">
+    <h1>FastForward Logistics Dashboard</h1>
+    <p>Dashboard will be built here</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.dashboard {
+  padding: 2rem;
+}
+</style>
